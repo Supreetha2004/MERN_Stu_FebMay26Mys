faa05649a69b6fb198a024b343e32299186ead5d
Supreetha2004 <supreethas745@gmail.com>
2026-03-03 22:17:15 +0530

    Arrow function

---
+++ W3/D2/P7.js
@@ -0,0 +1,16 @@
+//Arrow function
+(args) => {
+    //body of the function
+}
+
+//Add two numbers
+const sum = (a,b) => {
+    return a+b;
+}
+console.log("3+5=",sum(3,5));
+//Single line return/implicit return
+const square = x => x*x;
+console.log("Square of 44:",square(44));
+
+const sayHello = () => console.log("Hello from arrow func");
+sayHello();
