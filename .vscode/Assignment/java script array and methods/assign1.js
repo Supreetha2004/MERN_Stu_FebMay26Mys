75eb71947cc2a984e169884cd7ff6e7d4a04953c
Supreetha2004 <supreethas745@gmail.com>
2026-03-15 15:23:34 +0530

    employes records

--- .vscode/Assignment/java script array and methods/assign1.js
+++ .vscode/Assignment/java script array and methods/assign1.js
@@ -1,4 +1,4 @@
-let employeess = [
+let employeesss = [
     { name: "Asha", basePay: 30000, bonus: 2000, taxRate: 0.1 },
     { name: "Ravi", basePay: 25000, bonus: 1500, taxRate: 0.08 },
     { name: "Megha", basePay: -1000, bonus: 2000, taxRate: 0.1 },
