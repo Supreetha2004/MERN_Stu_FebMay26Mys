a7c7859c6b4f002d7dd61d530e23396041a4bfac
Supreetha2004 <supreethas745@gmail.com>
2026-03-16 16:29:26 +0530

    Node JS Architecture demo

---
+++ W5/D1/P3.js
@@ -0,0 +1,7 @@
+console.log("Node JS Architecture demo");
+console.log("1.Script started");
+//setTimeout
+setTimeout(()=>{
+     console.log("3.Timer callback finished after waiting");
+},10000);
+console.log("2.script continued without waiting for time callback");
