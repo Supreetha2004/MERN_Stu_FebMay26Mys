8445e5901730127156e1570d0d9994fef671f33d
Supreetha2004 <supreethas745@gmail.com>
2026-03-17 15:50:54 +0530

    Chaining promises with returned promises

--- W5/D2/P11.js
+++ W5/D2/P11.js
@@ -1,1 +1,29 @@
 // Chaining promises with returned promises
+function getOrderID(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501);
+    },500);
+});
+}
+function getOrderDetails(OrderID){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id:OrderID,
+                product:"labtop",
+                quantity:2
+            },700);
+        })
+    })
+}
+getOrderID()
+.then(function(OrderID){
+    console.log("orderId recevied:",OrderID);
+    return getOrderDetails(OrderID);
+})
+.then(function(OrderDetails){
+    console.log("order details looaded.");
+    console.log("product:",OrderDetails.product);
+    console.log("quantity:",OrderDetails.quantity);
+});
