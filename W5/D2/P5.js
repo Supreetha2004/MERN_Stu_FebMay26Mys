4d57715b8a953cb5d01451ab320fd1f97d3c5da5
Supreetha2004 <supreethas745@gmail.com>
2026-03-17 13:31:00 +0530

    callback error

---
+++ W5/D2/P5.js
@@ -0,0 +1,16 @@
+function divideNumbers(a, b, callback) {
+    if (b === 0) {
+        callback("cannot divide by zero.", null);
+        return;
+    }
+    const result = a / b;
+    callback(null, result);
+}
+
+divideNumbers(10, 0, function (error, result) {
+    if (error) {
+        console.log("Error:", error);
+        return;
+    }
+    console.log("Result:", result);
+});
