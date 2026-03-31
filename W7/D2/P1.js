02ffe502bbc871e25e40b455c9be269042fe146c
Supreetha2004 <supreethas745@gmail.com>
2026-03-31 16:06:48 +0530

    Routing base url and api/users

--- W7/D2/P1.js
+++ W7/D2/P1.js
@@ -1,24 +1,34 @@
-//Routing
-//"/" base url
-//"api/users"
+/**Routing
+ *  "/" base url
+ * "/api/users"
+ */
+
 const express = require("express");
 const app = express();
-//"api/users"/create/delete/update/:id
-//Router objects help organize route Group
+
+//"/api/users" /create /delete /update /:id
+
+// Router objects help organize route Groups
+
 const apiRouter = express.Router();
-apiRouter.get("/user",function(req,res){
+
+apiRouter.get("/users",function(req,res){
     res.json({
         route:"/api/users",
-        message:"User route inside apl router"
+        message: "Users route inside api router"
     });
 });
+
 apiRouter.get("/orders",function(req,res){
     res.json({
         route:"/api/orders",
-        message:"User route inside apl router"
+        message: "orders route inside api router"
     });
 });
+
+//Mount the router under the /api base path
 app.use("/api",apiRouter);
+
 app.listen(4000,function(){
     console.log("Express server running at http://localhost:4000");
 });
