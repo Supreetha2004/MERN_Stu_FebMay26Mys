d5a7d3c7ae71ae2a85710aa6301d695d53b55489
Supreetha2004 <supreethas745@gmail.com>
2026-04-07 09:52:08 +0530

    To cancel the existing booking if exists

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
@@ -1,31 +1,39 @@
 //cancel.js
-//To cancel the existing booking if exists
+// To cancel the existing booking if exists
 const bookingEmitter = require("./events");
 const {getCurrentBooking,clearCurrentBooking} = require("./booking");
+
 function cancelBooking(movies){
     const booking = getCurrentBooking();
 
-    if(!booking) {
-        bookingEmitter.emit("bookingfailed","No booking found  to cancel.");
+    if (!booking) {
+        bookingEmitter.emit("bookingFailed","No booking found to cancel.");
         return null;
     }
-     const movie = movies.find((m)=>m.id === booking. movieId);
-     if(!movie) {
-        bookingEmitter.emit("bookingFailed","Movie data not found while canceling booking.");
+
+    const movie = movies.find((m)=>m.id === booking.movieId);
+    if (!movie) {
+        bookingEmitter.emit("bookingFailed","Movie data not found while cancelling booking.");
         return null;
-     }
-     const showTime = movie.showtimes.find((show)=>show.time.toLowerCase()=== booking.time.toLowerCase());
-     if(!showTime) {
-        bookingEmitter.emit("bookingfailed","showtime data not found");
+    }
+
+    const showtime = movie.showtimes.find((show)=>show.time.toLowerCase()===booking.time.toLowerCase());
+    if (!showtime) {
+        bookingEmitter.emit("bookingFailed","Showtime data not found");
         return null;
-     }
-     //restore seats
-     showTime.seatAvailable +=booking.seatCount;
-     //clear currentbooking
-     clearCurrentBooking();
-     bookingEmitter.emit("bookingCancelled",booking);
-     return booking;
+    }
+
+    //restore seats
+    showtime.seatsAvailable +=booking.seatCount;
+
+    // clear current Booking
+    clearCurrentBooking();
+
+    bookingEmitter.emit("bookingCancelled",booking);
+
+    return booking;
 }
+
 module.exports = {
     cancelBooking
 };
