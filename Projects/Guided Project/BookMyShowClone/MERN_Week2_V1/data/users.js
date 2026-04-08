e3af0727414023f4f5531509f80b89d76cddf90d
Supreetha2004 <supreethas745@gmail.com>
2026-04-08 10:07:01 +0530

    user's details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,18 @@
+//user's details of the application
+const users = [
+    {
+        id:1,
+        name:"Rakesh",
+        email:"u@e.com",
+        password:"mahi123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    },
+];
+module.exports = users;
