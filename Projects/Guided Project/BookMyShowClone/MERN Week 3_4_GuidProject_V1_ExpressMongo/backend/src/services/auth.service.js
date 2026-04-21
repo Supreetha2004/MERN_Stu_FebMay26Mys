5f294a32ccdb03fdf7b7c595b8c03401bb4722c4
Supreetha2004 <supreethas745@gmail.com>
2026-04-21 14:56:49 +0530

    Authorization related service function is created

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/auth.service.js
@@ -0,0 +1,62 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpService = require("./otp.service");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+//Register user
+exports.registerUser = async({name,email,password}) =>{
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        throw new Error("User already exists");
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+    await otpService.generateOTP(email);
+    return {email:user.email};
+};
+//verify otp
+exports.verifyOTP = async({email,otp})=>{
+    const record = await OTP.findOne({email}).select("+otp");
+    if(!record){
+        throw new Error("OTP expired or not found");
+    }
+    const isMatch = await bcrypt.compare(otp,record.otp);
+    if(!isMatch){
+        record.attempts +=1;
+        await record.save();
+        throw new Error("Invalid OTP");
+    }
+    await User.updateOne({email},{isVerified:true});
+    return true;
+};
+//Login
+exports.loginUser = async ({email,password}) => {
+    const User = await User.findOne({email}).select("+password");
+    if(!user){
+        throw new Error("User not found");
+    }
+    if(!user.isVerified){
+        throw new Error("User not verified");
+    }
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch){
+        throw new Error("Invalid credentials");
+    }
+    const token = jwt.sign(
+        {id:user_id,role:user.role},
+        process.env.JWT_SECRET,
+        {expiresIn:"id"}
+    );
+    return{
+        token,
+        user:{
+             id:user.user_id,
+             role:user.role
+        },
+    };
+};
+        
