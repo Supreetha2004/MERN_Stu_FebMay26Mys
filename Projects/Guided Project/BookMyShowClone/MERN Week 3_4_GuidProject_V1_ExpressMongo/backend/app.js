9848e607b094ff664021a2bf54fc8c4c40356c85
Supreetha2004 <supreethas745@gmail.com>
2026-04-22 09:18:27 +0530

    Added auth routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const authRoutes = require("./routes/auth.routes")
+const authRoutes = require("./src/routes/auth.routes");
+
 const app = express();
+
 app.use(express.json());
-app.use("api/auth",authRoutes);
+app.use("/api/auth",authRoutes);
+
+//Base URL
 app.get("/",(req,res)=>{
-     res.status(200).json({
+    res.status(200).json({
         success:true,
-        message:"Movie booking API is runing...."
-   });
+        message: "Movie booking API is running...",
+    });
 });
-module.exports=app;
+
+
+module.exports = app;
