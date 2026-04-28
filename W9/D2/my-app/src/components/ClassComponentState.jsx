a0a777c2f78b236f43876cddd668f01192405907
Supreetha2004 <supreethas745@gmail.com>
2026-04-28 13:09:25 +0530

    class component created with the concept of prrops.constructor,state, and event handler

--- W9/D2/my-app/src/components/ClassComponentState.jsx
+++ W9/D2/my-app/src/components/ClassComponentState.jsx
@@ -1,4 +1,26 @@
-import {React} from "react";
+import {React,Component} from "react";
 export class ClassComponentState extends Component{
     //constructor
+    constructor(props){
+        super(props);//call parent constructor frist
+        this.state = {count:0};//state is going to be here and stays even after the re-renders
+
+    }
+    //2.Event handler :arrow function to handle 'this'binding
+    increment = () => {
+        this.setState((prevState)=>({
+            count:prevState.count + 1
+        }));
+    };
+    //3.Render:state/props change
+    render(){
+        console.log("render() called");
+    return(
+        <div>
+        <h3>class Component state</h3>
+        <p>Count:{this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        </div>
+    )
+    }
 }
