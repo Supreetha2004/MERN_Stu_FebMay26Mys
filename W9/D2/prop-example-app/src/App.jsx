b484f61966eb41c70ce9c78346560cf6dc33be3d
Supreetha2004 <supreethas745@gmail.com>
2026-04-29 11:44:11 +0530

    state lifiting

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import { PropBasics } from './components/P1'
 import { PropDestructuring } from './components/P2'
+import { PropsChildren } from './components/P3'
+import { PropDrillingDemo } from './components/P4'
+import { SharedStateParent } from './components/P5'
 function App() {
 return(
   <>
   {/* <PropBasics/> */}
-  <PropDestructuring/>
+  {/* <PropDestructuring/> */}
+  {/* <PropsChildren/> */}
+  {/* <PropDrillingDemo /> */}
+  <SharedStateParent />
   </>
 )
 }
