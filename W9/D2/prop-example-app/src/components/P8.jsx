2c322b61d380bad25c72965245bfa26449ef5c78
Supreetha2004 <supreethas745@gmail.com>
2026-04-29 15:06:51 +0530

    Props immutable

---
+++ W9/D2/prop-example-app/src/components/P8.jsx
@@ -0,0 +1,14 @@
+// Props immutable
+function Child({message}){
+    return <p>Received message: {message}</p>;
+}
+
+export function PropsImmutability(){
+    const ParentMessage = "Props are read-only";
+    return(
+        <>
+        <h2>Props are immutable</h2>
+        <Child message={ParentMessage}/>
+        </>
+    )
+}
